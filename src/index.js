05b362aead07084f4df98d1c8cde5e6aff927c33
LenaYork <lenayork@tut.by>
2021-02-23 19:59:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    
+    let array = String(n).split("");
+
+    if (array[0] === "-")  {
+        array.shift();
+    }
+
+    return Number(array.reverse().join(''));
 }
